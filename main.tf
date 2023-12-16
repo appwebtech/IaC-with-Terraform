45e26604eb6dc271afbc69e4dec66275962756af
appwebtech <josembi@gmail.com>
2023-12-16 21:13:03 +0000

    Resolv rebase conflict

--- main.tf
+++ main.tf
@@ -11,3 +11,40 @@ module "aws-web-bucket" {
   source         = "./modules/aws-s3-web-bucket"
   s3_bucket-name = "${var.unique-bucket-name.name}-${var.unique-bucket-name.env}-${random_uuid.my-long-unique-name.result}"
 }
+
+# S3 bucket logs, ACL, & ownership controls for Cloudfront CDN
+
+
+resource "aws_s3_bucket" "cdn-website-logs" {
+  bucket = "${var.s3-bucket-logs.name}-${var.s3-bucket-logs.purpose}-${var.s3-bucket-logs.website}.${random_id.loggy.hex}"
+
+  tags = {
+    name    = var.s3-bucket-logs["name"]
+    purpose = var.s3-bucket-logs["purpose"]
+    website = var.s3-bucket-logs["website"]
+  }
+}
+
+resource "aws_s3_bucket_logging" "cdn-website" {
+  bucket = module.aws-web-bucket.name
+
+  target_bucket = aws_s3_bucket.cdn-website-logs.id
+  target_prefix = "logs/cdn"
+}
+
+resource "aws_s3_bucket_acl" "cdn-bucket-acl" {
+  bucket = aws_s3_bucket.cdn-website-logs.id
+  acl    = "private"
+  depends_on = [
+    aws_s3_bucket_ownership_controls.cdn-bucket-acl-ownership
+  ]
+}
+
+resource "aws_s3_bucket_ownership_controls" "cdn-bucket-acl-ownership" {
+  bucket = aws_s3_bucket.cdn-website-logs.id
+  rule {
+    object_ownership = "ObjectWriter"
+  }
+
+
+}
