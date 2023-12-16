90ae86233d61451393c15bc5258983ba807a54e4
appwebtech <josembi@gmail.com>
2023-12-16 20:27:11 +0000

    Update variables file

--- variables.tf
+++ variables.tf
@@ -1,4 +1,4 @@
-variable "region" {
+variable "my_region" {
   type        = string
   description = "Your closest AWS region"
 }
@@ -17,8 +17,24 @@ variable "resource_tags" {
 variable "unique-bucket-name" {
   type = map(string)
   default = {
-    name = "your-bucket-name"
-    env  = "prod"
+    name = "www"
+    env  = "josephmwania"
   }
   description = "A unique bucket name with a randomized suffix"
+}
+
+variable "website-domain-name" {
+  type        = string
+  description = "website domain name"
+}
+
+# S3 Bucket logs for Cloudfront CDN Variables
+variable "s3-bucket-logs" {
+  type = map(string)
+  default = {
+    name    = "cdn"
+    purpose = "logs"
+    website = "joseph-resume"
+  }
+  description = "logging bucket for hosted S3 bucket"
 }
