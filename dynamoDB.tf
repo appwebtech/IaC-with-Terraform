2dece64f551857393cfae9bab81b90c87f92454a
appwebtech <josembi@gmail.com>
2023-12-20 14:43:34 +0000

    Add IAM policies & Lambda Fn

--- dynamoDB.tf
+++ dynamoDB.tf
@@ -9,6 +9,53 @@ resource "aws_dynamodb_table" "counter_table" {
   }
 }
 
+# DynamoDB Policy
+resource "aws_iam_policy" "dynamodb_policy" {
+  name        = "DynamoDBPolicy"
+  description = "IAM policy for DynamoDB access"
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Action = [
+        "dynamodb:UpdateItem",
+        "dynamodb:PutItem",
+        "dynamodb:GetItem"
+      ],
+      Effect   = "Allow",
+      Resource = aws_dynamodb_table.counter_table.arn
+    }]
+  })
+}
+
+# DynamoDB Role
+resource "aws_iam_role" "counter_lambda_role" {
+  name = var.lambda_role
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Action = "sts:AssumeRole",
+      Effect = "Allow",
+      Principal = {
+        Service = "lambda.amazonaws.com"
+      }
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "dynamodb_attachment" {
+  policy_arn = aws_iam_policy.dynamodb_policy.arn
+  role       = aws_iam_role.counter_lambda_role.name
+}
+
+
+resource "aws_iam_policy_attachment" "counter_lambda_policy_attachment" {
+  name       = var.lambda_attachment
+  policy_arn = aws_iam_policy.dynamodb_policy.arn
+  roles      = [aws_iam_role.counter_lambda_role.name]
+}
+
 # Lambda
 resource "aws_lambda_function" "counter_lambda" {
   filename      = "counter_lambda.zip"
@@ -25,17 +72,7 @@ resource "aws_lambda_function" "counter_lambda" {
   }
 }
 
-resource "aws_iam_role" "counter_lambda_role" {
-  name = var.lambda_role
 
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [{
-      Action = "sts:AssumeRole",
-      Effect = "Allow",
-      Principal = {
-        Service = "lambda.amazonaws.com"
-      }
-    }]
-  })
-}
+
+
+
